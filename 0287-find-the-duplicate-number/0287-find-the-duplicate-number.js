aded5b19768f3e1ab1e5aead52bff8bbdea3efc3
Andrew Ihn <andrewihn11@gmail.com>
2023-06-03 14:48:28 -0700

    Time: 137 ms (27.78%), Space: 56.5 MB (30.93%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -4,7 +4,14 @@
  */
 var findDuplicate = function(nums) {
     const cow = {};
-    let result = 0;
-    nums.forEach(num => cow[num] ? result = num : cow[num] = 1);
-    return result;
+    for (let i = 0; i < nums.length; i++) {
+        if (cow[nums[i]]) {
+            return nums[i];
+        } else {
+            cow[nums[i]] = 1;
+        }
+    }
+    // let result = 0;
+    // nums.forEach(num => cow[num] ? result = num : cow[num] = 1);
+    // return result;
 };
