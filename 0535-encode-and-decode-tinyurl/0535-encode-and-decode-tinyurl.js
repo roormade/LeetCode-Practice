288ed9d8b038b46758cc57810a160167ea707061
Andrew Ihn <andrewihn11@gmail.com>
2023-06-11 16:01:03 -0700

    Time: 63 ms (77.14%), Space: 43.8 MB (60.00%) - LeetHub

---
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.js
@@ -0,0 +1,50 @@
+/**
+ * Encodes a URL to a shortened URL.
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+// const rdmStr = function() {
+//     const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+//     let rdm = '';
+//     for (let i = 0; i < 6; i++) {
+//         rdm += chars[Math.floor(Math.random() * chars.length)];
+//     }
+//     return rdm;
+// };
+
+const mapE = new Map();
+const mapD = new Map();
+
+var encode = function(longUrl) {    
+    if (mapE.has(longUrl)) {
+        return mapE.get(longUrl);
+    }
+    
+    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let tinyUrl = '';
+    do {
+        tinyUrl = '';
+        for (let i = 0; i < 6; i++) {
+            tinyUrl += chars[Math.floor(Math.random() * chars.length)];
+        }        
+    } while (mapD.has(tinyUrl));
+    mapE.set(longUrl, tinyUrl);
+    mapD.set(tinyUrl, longUrl);
+    return tinyUrl;
+};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ *
+ * @param {string} shortUrl
+ * @return {string}
+ */
+var decode = function(shortUrl) {
+    return mapD.get(shortUrl);        
+};
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(url));
+ */
