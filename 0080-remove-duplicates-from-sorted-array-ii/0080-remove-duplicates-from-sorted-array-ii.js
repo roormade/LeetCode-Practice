0b45bf97fe061e5e2a836084b8575b0af50c9a67
Andrew Ihn <andrewihn11@gmail.com>
2023-09-19 15:53:51 -0700

    Time: 61 ms (57.11%), Space: 44.2 MB (43.21%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -3,9 +3,6 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    // if (nums.length < 3) {
-    //     return nums.length;
-    // }
     let currI = 2;
     for (let i = 2; i < nums.length; i++) {
         if (nums[i] !== nums[currI - 2]) {
