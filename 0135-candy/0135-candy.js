7204590e7f39fb7b4de098e3293f632a9bf3fa23
Andrew Ihn <andrewihn11@gmail.com>
2023-10-02 15:23:36 -0700

    Time: 58 ms (81.31%), Space: 45.3 MB (53.03%) - LeetHub

---
+++ 0135-candy/0135-candy.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} ratings
+ * @return {number}
+ */
+var candy = function(ratings) {
+    const lng = ratings.length;
+    const alCnd = new Array(lng).fill(1);
+    for (let i = 1; i < lng; i++) {
+        if (ratings[i] > ratings[i - 1]) {
+            alCnd[i] = alCnd[i - 1] + 1;
+        }
+    }
+    for (let i = lng - 2; i >= 0; i--) {
+        if (ratings[i] > ratings[i + 1]) {
+            alCnd[i] = Math.max(alCnd[i], alCnd[i + 1] + 1);
+        }
+    }
+    return alCnd.reduce((a, b) => a + b);
+};
