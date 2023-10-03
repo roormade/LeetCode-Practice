b62048c435e8b7f9f6ca29c527bafaf5341f92bf
Andrew Ihn <andrewihn11@gmail.com>
2023-10-02 18:23:06 -0700

    Time: 102 ms (77.92%), Space: 47.7 MB (36.23%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -4,13 +4,13 @@
  */
 var romanToInt = function(s) {
     const dct = {
-        M: 1000,
-        D: 500,
-        C: 100, 
-        L: 50,
-        X: 10,
-        V: 5, 
         I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
     }
     let ttlInt = dct[s[0]];
     for (let i = 1; i < s.length; i++) {
